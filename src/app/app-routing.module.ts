f3e08f613d4ae1d85d4a273682733c2f623125f4
MarijnvanderZon <71638156+marijnvanderzon-03@users.noreply.github.com>
2022-01-20 14:09:24 +0100

    made it so you need to login for main shop page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/hoofdpagina', pathMatch: 'full'},
-  {path: 'hoofdpagina', component: MainComponent},
+  {path: 'hoofdpagina', component: MainComponent, canActivate: [AuthGuardService]},
   {path: 'moderator', component: ModeratorComponent, canActivate: [RoleGuardService], data: {expectedRole: 'admin'},},
   {path: 'login', component: LoginComponent}
 ];
